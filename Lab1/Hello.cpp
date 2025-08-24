cd6cfeb790969c547c3ea413b202b2d888e78def
charleskhu675 <charleskhu675@gmail.com>
2025-08-23 22:16:54 -0700

    saved file

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {Setting up IDE & Compiler}
+ ***/
+
+#include <iostream>
+ #include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
