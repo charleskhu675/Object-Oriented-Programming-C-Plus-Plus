ffbe9aa2c89982db2a76d6b5333f447996cb296e
charleskhu675 <charleskhu675@gmail.com>
2025-09-10 15:40:52 -0700

    vector2

--- Labs/lab2/vector.cpp
+++ Labs/lab2/vector.cpp
@@ -7,4 +7,19 @@ using namespace std;
  * 
  * @param v - vector of integers
  */
-void printMemVec(){}
+void printMemVec(){
+
+}
+
+/**
+ * @brief increments all of the elements in v by 10
+ * 
+ * @param v - address to a vector of integers
+ */
+void incVecBy10(){
+
+}
+
+int main() {
+    const int SIZE = 5;
+}
