884a0d2b33b28473d6f653d99bf3a1f4b8612381
charleskhu675 <charleskhu675@gmail.com>
2025-09-10 15:38:28 -0700

    vector1

---
+++ Labs/lab2/vector.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * @brief prints the elements in the vector and their memory locations
+ * 
+ * @param v - vector of integers
+ */
+void printMemVec(){}
