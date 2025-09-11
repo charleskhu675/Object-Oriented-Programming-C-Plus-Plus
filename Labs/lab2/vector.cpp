c78720b827c07eb9bb25fea6ea2b2627152c3675
charleskhu675 <charleskhu675@gmail.com>
2025-09-10 22:50:37 -0700

    Update vector.cpp

--- Labs/lab2/vector.cpp
+++ Labs/lab2/vector.cpp
@@ -7,8 +7,11 @@ using namespace std;
  * 
  * @param v - vector of integers
  */
-void printMemVec(){
-
+void printMemVec(const vector<int>& v){
+    cout << "Vector - Each int is worth " << sizeof(v[0]) << " bytes\n";
+    for (int i = 0; i < v.size(); i++) {
+        cout << "Value: " << v[i] << " at Memory Location: " << &v[i] << "\n";
+    }
 }
 
 /**
@@ -16,10 +19,37 @@ void printMemVec(){
  * 
  * @param v - address to a vector of integers
  */
-void incVecBy10(){
-
+void incVecBy10(vector<int>& v){
+    for (int i = 0; i < v.size(); i++) {
+        v[i] += 10;
+    }
 }
 
 int main() {
     const int SIZE = 5;
+    vector<int> vec(SIZE);
+    for(int i = 0; i < SIZE; i++) {
+        vec[i] = 100 + i;
+    }
+
+    printf("Before Increment-------------\n");
+    printMemVec(vec);
+
+    incVecBy10(vec);
+
+    printf("After Increment--------------\n");
+    printMemVec(vec);
+
+    vec.pop_back();
+
+    printf("After Pop-------------------\n");
+    printMemVec(vec);
+
+    vec.push_back(101);
+    vec.push_back(102);
+
+    printf("After Push------------------\n");
+    printMemVec(vec);
+
+    return 0;
 }
