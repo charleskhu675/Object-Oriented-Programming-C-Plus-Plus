98d3962dce9401b6cd7abe06886e58acb5068177
charleskhu675 <charleskhu675@gmail.com>
2025-10-20 01:21:37 -0700

    space

--- Labs/lab5/Pokemon.cpp
+++ Labs/lab5/Pokemon.cpp
@@ -39,7 +39,7 @@ void Pokemon::speak(){
 }
 
 void Pokemon::printStats(){
-    printf("Name:%s\t HP: %i\t DEF: %i\t ATT: %i\n", name.c_str(), hp, defense, attack);
+    printf("Name: %s\t HP: %i\t DEF: %i\t ATT: %i\n", name.c_str(), hp, defense, attack);
     cout << "Type: ";
     for(int i = 0; i < type.size(); i++){
         cout << type[i] << "\t";
