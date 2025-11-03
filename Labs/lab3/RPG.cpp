f2d81e784d1bb57b3fbc6df6e0adb405dda8b569
charleskhu675 <charleskhu675@gmail.com>
2025-11-03 00:57:43 -0800

    reassign lab4

--- Labs/lab3/RPG.cpp
+++ Labs/lab3/RPG.cpp
@@ -1,7 +1,6 @@
 //RPG.cpp
 #include <iostream>
 #include <string>
-#include <random>
 #include "RPG.h"
 using namespace std;
 
@@ -34,47 +33,6 @@ bool RPG::isAlive() const {
     return hits_taken < MAX_HITS_TAKEN;
 }
 
-void RPG::setName(string name) {
-    this->name = name;
-}
-
-void RPG::updateExpLevel() {
-    exp += 50.0;
-    if (exp >= 100.0) {
-        level++;
-        exp = 0.0;
-        luck += 0.1;
-    }
-}
-
-void RPG::attack(RPG * opponent) {
-    random_device rd;
-    mt19937 gen(rd()); // seed with random
-    uniform_real_distribution<double> dis(0.0, 1.0);
-
-    float random_num = dis(gen);
-
-    bool hit = (random_num > HIT_FACTOR * opponent->getLuck());
-
-    if (hit) {
-        opponent->setHitsTaken(opponent->getHitsTaken() + 1);
-    }
-}
-
-void RPG::printStats() {
-    cout << "Name: " << name
-         << "\tHits Taken: " << hits_taken
-         << "\tLuck: " << luck
-         << "\tExp: " << exp
-         << "\tLevel: " << level
-         << "\tStatus: " << (isAlive() ? "Alive" : "Dead")
-         << endl;
-}
-
-RPG::~RPG() 
-{
-}
-
 //Accessor functions
 string RPG::getName() const {
     return name;
@@ -95,5 +53,3 @@ float RPG::getExp() const {
 int RPG::getLevel() const {
     return level;
 }
-
-
